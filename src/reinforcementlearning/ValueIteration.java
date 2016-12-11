59401ec7f9e9fe71e0fb148620f62cc5c459be26
fippy24 <connor.trumpet@gmail.com>
2016-12-10 20:58:44 -0700

    This is a possible finished first draft
    I have not ran this shit yet so it might suck

--- src/reinforcementlearning/ValueIteration.java
+++ src/reinforcementlearning/ValueIteration.java
@@ -9,21 +9,57 @@ import java.util.ArrayList;
  */
 public class ValueIteration extends Algorithm{
     Random random = new Random();
+    ArrayList<int[]> values;
 
     public ValueIteration(RaceTrack t, RaceCar c, TrackGUI g) {
         super(t, c, g);
-        ArrayList<int[]> values = initializeStateValues();
+        values = initializeStateValues();
     }
     
     public int[] findNextMove(){
         int[] accl = {0,0};
+        int[] carState = {c.getXPos(),c.getYPos(),c.getXVel(),c.getYVel()}
+        int[] state, nextState;
+        for(state:values){
+            if(matches(state))
+                break;
+        }
+        int currVal = state[4];
+        RaceCar car = new RaceCar(c.getXPos(),c.getXPos());
+        int[] maxState = {0,0,0,0,0};
+        // Find each possible next state based on possible accelerations and select best acceleration
+        for(int accX = -1; accX<2; accX++){
+            for(int accY = -1; accY<2; accY++){
+                
+                car.setVel(c.getXVel()+accX,c.getYVel()+accY);
+                car.setPos(c.getXVel()+accX+c.getXPos(),c.getYVel()+accY+c.getYPos());
+                
+                for(nextState:values){
+                    if(matches(car, nextState)){
+                        if(state[4]>maxState[4]){ // best next value
+                            maxState=nextState;
+                            accl={accX,accY);
+                        }
+                    break;
+                    }
+                }
+            }
+        }
         
-        // TO-DO implement algorithm
+        // Calculate and set updated state value
+        int gamma = .5;
+        int[] newState = {state.getXPos(),state.getYPos(),state.getXVel(),state.getYVel(), state[4]*gamma*maxState[4]}
+        values.set(values.indexOf(state),newState);
+        
         
         return accl;
     }
     
-    
+    public boolean matches(RaceCar car, int[] state){
+        if (state[0]==car.getXPos() && state[1]==car.getYPos() && state[2]==car.getXVel() && state[3]==car.getYVel()){
+            return true;
+        return false;
+    }
     
     public String toString(){
         return "Value Iteration " + super.toString();
@@ -54,7 +90,7 @@ public class ValueIteration extends Algorithm{
                 }
             }
         }
-                
+        return values;     
     }
 }
 
@@ -66,10 +102,10 @@ know a state’s true value; we only know its immediate reward. But, for example
 a state might have low initial reward but be on the path to a high-reward state.
 The true value (or utility) of a state is the immediate reward for that state, plus the expected
 discounted reward if the agent acted optimally from that point on:
-U(s)=R(s)+γmaxa􏰀s′ T(s,a,s′)U(s′)
-Note that the value for each state can potentially depend on all of its neigh- bors’ values. If our
+U(s)=R(s)+γmaxa􏰀s′ U(s′)
+Note that the value for each state can potentially depend on all of its neighbors’ values. If our
 state space is acyclic, we can use dynamic programming to solve this. Otherwise, we use value iteration.
-3
+
 1. Assign each state a random value
 2. For each state, calculate its new U based on its neighbor’s utilities.
 3. Update each state’s U based on the calculation above: Ui+1(s)=R(s)γmaxs′ U(s′)
